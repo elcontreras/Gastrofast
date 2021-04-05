946b06465796d5741dd96e6ecb052c348b0de6ae
Luis <elcontreras@uninorte.edu.co>
2021-04-05 16:39:11 -0500

    first commit

---
+++ flutter_login_ui-master/lib/utilities/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle2 = TextStyle(
+  color: Color(0xFFFA6400),
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFFE0CA6E),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [],
+);
